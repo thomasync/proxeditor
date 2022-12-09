38be89126f7153d3faa0bb43632ee69feb602105
Thomas <github@thomascauquil.fr>
2022-12-09 07:15:28 +0100

    fix(0.0.2) connexion from others networks

--- proxy.sh
+++ proxy.sh
@@ -16,8 +16,8 @@ fi
 
 if test "$production" = "true"; then
     echo "Run in production mode."
-    mitmdump -s proxy.py --listen-host 0.0.0.0 --listen-port 8302 --ssl-insecure --set confdir=/proxy/.certs $proxyauth
+    mitmdump -s proxy.py --listen-host 0.0.0.0 --listen-port 8302 --ssl-insecure --set confdir=/proxy/.certs --set block_global=false $proxyauth
 else
     echo "Run in development mode."
-    nodemon --watch "**/*" --ext py,sh --exec "pkill -HUP mitmweb; mitmweb -s proxy.py --listen-host 0.0.0.0 --listen-port 8302 --web-host 0.0.0.0 --web-port 8301 --ssl-insecure --set confdir=/proxy/.certs --no-web-open-browser $proxyauth"
+    nodemon --watch "**/*" --ext py,sh --exec "pkill -HUP mitmweb; mitmweb -s proxy.py --listen-host 0.0.0.0 --listen-port 8302 --web-host 0.0.0.0 --web-port 8301 --ssl-insecure --set confdir=/proxy/.certs --no-web-open-browser --set block_global=false $proxyauth"
 fi
